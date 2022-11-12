bea379c5eb69682a1d0baa05a6c317ff9302b921
3103ski <bjastski@gmail.com>
2022-11-12 14:55:22 -0800

    adds react router dom and creates a second page

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,34 @@
 import React from 'react';
+import { BrowserRouter as Router, Routes, Route, NavLink } from 'react-router-dom';
 
 // Pages
 import ProjectsPage from './projects/ProjectsPage';
+import HomePage from './HomePage';
 
 import './App.css';
 
 function App() {
 	return (
-		<div className='container'>
-			<ProjectsPage />
-		</div>
+		<Router>
+			<header className='sticky'>
+				<span>
+					<img src='/assets/logo-3.svg' alt='logo' width={49} height={99} />
+				</span>
+				<NavLink to='/' className='button rounded'>
+					<span className='icon-home'></span>
+					Home
+				</NavLink>
+				<NavLink to='/projects' className='button rounded'>
+					Projects
+				</NavLink>
+			</header>
+			<div className='container'>
+				<Routes>
+					<Route element={<HomePage />} path='/' />
+					<Route element={<ProjectsPage />} path='/projects' />
+				</Routes>
+			</div>
+		</Router>
 	);
 }
 
